81417c35eeb4ea82b251c1ba87bf1aac1f34ee71
Anandhbasha <anadhakm@gmail.com>
2025-11-26 19:31:13 +0530

    basics Rathna

---
+++ index.py
@@ -0,0 +1,25 @@
+'''print("Hello Python", end='-')
+print(100)
+print("red","blue","green","black",sep='/') '''
+
+# variable
+# a=20
+# print(a)
+# print(type(a))
+
+# b=30
+# b=60
+# print(b)
+
+# a,b,c=30,40,80
+
+# temp = a
+# a=b
+# b=c
+# c=temp
+# print(a,b,c)
+
+# a,b,c = b,c,a
+# print(a,b,c)
+
+
