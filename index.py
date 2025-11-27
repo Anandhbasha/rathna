0918adc25a0fc4e7312cde944aad2e10c42f2966
Anandhbasha <anadhakm@gmail.com>
2025-11-27 17:49:30 +0530

    operators

--- index.py
+++ index.py
@@ -22,4 +22,63 @@ print("red","blue","green","black",sep='/') '''
 # a,b,c = b,c,a
 # print(a,b,c)
 
+# data types
+# simple data types(Premitive Data types)
+# int
+# a=10
+# print(type(a))
+# # float
+# a=20.5
+# print(type(a))
+# # string
+# name = "xyx"
+# print(type(name))
+# # boolean
+# isComing = True
+# print(type(isComing))
+# complex data types(Non Premitive)
+#list
+list = [10,10.5,"abc",True] #index 
+# print(type(list))
+# print(list)
+#0 = 10
+#1 = 10.5
+#2 = "abc"
+#3 = True
+# print(list[0])
+# print(list[1])
+list[3] = 100
+# print(list)
+# # tuple
+# tuple = (10,20,30,40,True)
+# print(tuple)
+# tuple[3] = 200
+print(tuple[0])
+# set
+sets = {101,101,102,108,102,103,105,106,107,104}
+print(sets)
+# dictionary
+person = {
+    "name":"abc",
+    "age":20,
+    "city":"CBE",
+    "family":{
+        "Father":"ghrr",
+        "mother":"hgghhg",
+        "siblings":{
+            "brother":"jkhjh",
+            "sister":"hjghg"
+        }
+    }
+}
+# print(person["family"]["siblings"])
+print(type(person))
 
+import json 
+newJson = json.dumps(person)
+print(newJson)
+print(type(newJson))
+
+
+dictValue = json.loads(newJson)
+print(type(dictValue))
---
+++ operators.py
@@ -0,0 +1,44 @@
+# arithmetic operator
+# +,-,*,/,//,**,%
+
+num1 = 10
+num2 = 10
+# print(num1+num2)
+# print(num1-num2)
+# print(num1*num2)
+# print(num1/num2)
+# print(num1//num2)
+# print(num1%num2)
+# print(num1**num2)
+
+# assignment
+# =,+=,-+,*=
+x=20
+x+=10
+
+# comparison
+# >,<,<=,>=,!=
+
+# print(num1>num2)
+# print(num1<num2)
+# print(num1<=num2)
+# print(num1>=num2)
+# print(num1!=num2)
+
+# logical Operators
+# and or not
+# print(10>5 and 15<8 and 7>5)
+# print(10<5 or 15<8 or 7<5)
+# print(not num1==num2)
+
+# membership 
+
+list1 = [10,20,30]
+print(22 in list1)
+
+# identity
+a=[1,2,3]
+b=a
+c= [1,2,3]
+print(a is b)
+print(a is c)
