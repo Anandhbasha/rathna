244aefcb651d45dc989663fa7ac7b85b58c6556e
Anandhbasha <anadhakm@gmail.com>
2025-12-01 18:12:27 +0530

    conditional

---
+++ conditional.py
@@ -0,0 +1,21 @@
+# if
+age=17
+if age>=18:
+    print("Eligible to vote")
+else:
+    print("Not Eligible")
+
+# adult
+# Tennager
+# kids
+
+# age = 20
+
+age = int(input("Enter your Age:"))
+if age>19:
+    print("He is Adult")
+elif age<13:
+    print("He is kids")
+else:
+    print("He is Teenager")
+
---
+++ functions.py
@@ -0,0 +1,46 @@
+# def add():
+#     print("Hello")
+
+# add()
+
+# def add(a,b):
+#     print(a+b)
+# add(60,70)
+# add(66,70)
+# add(60,80)
+# add(62,75)
+# add(60,70)
+
+# return
+# def add(a,b):
+#     return a+b
+
+# print(add(60,70))
+
+# def user(name,age):
+#     print(f'The user Name is:{name} and the age is {age}')
+
+# user(age=30,name="abc")
+
+# args
+def add(*num1):  # num1 = (70,50,60)
+    total = 0
+    for i in num1:
+        total+=i
+    print(total)
+
+add(70,50,60)
+add(50,80,99,66)
+
+
+# kwargs
+# def userInfo(**users):
+#     for key,value in users.items():
+#         print(key,value)
+
+# userInfo(name="arun",age=25,city="CBE")
+# userInfo(name="arun",age=25,city="CBE",course="Python")
+
+# lambda
+square = lambda x,y:x*y
+print(square(5,10))
