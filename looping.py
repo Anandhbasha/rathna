14a4ceef195513a37aa6da786dfe8cae1e30b3d0
Anandhbasha <anadhakm@gmail.com>
2025-12-02 18:00:47 +0530

    loops

---
+++ looping.py
@@ -0,0 +1,60 @@
+# for i in range(5):
+#     print(i)
+
+
+# # list 
+# arr = [10,20,30]
+# for i in arr:
+#     print(i)
+
+# # string
+# name = "ajay"
+# for i in name:
+#     print(i)
+
+# for i in  range(1,3):
+#     for j in range(1,6):
+#         print(f'{i}X{j} = {i*j}')
+#     print("---")
+
+
+
+# for i in range(1,11,2):
+#     print(i)
+
+
+
+# while
+count = 1
+while count<=5:
+    print("Count:",count)
+    count+=1
+
+# break
+
+# for i in range(1,10):
+#     if i==5:
+#         break
+#     print(i)
+# continue
+# for i in range(1,6):
+#     if i%2==0:
+#         continue
+#     print(i)
+# pass
+
+for i in range(1,6):
+    # pass
+    print(i)
+
+
+# four digit number to get input ex:4321 output 10
+# pyramid pattern
+#    *
+#   ***
+#  *****  one for loop
+
+# reverse input value  
+# check palindrome or not
+
+# range 10,9,8,7...1
